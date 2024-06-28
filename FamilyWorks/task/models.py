ac24261622c7e7b970cbca741e580b6945c834e1
Andrew <andrewvo1024@gmail.com>
2024-06-28 16:00:18 -0500

    task app made and model migrated

---
+++ FamilyWorks/task/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+
+User = get_user_model()
+
+# Create your models here.
+class Task(models.Model):
+    class StatusChoices(models.TextChoices):
+        OPEN = 'open', 'Open'
+        IN_PROGRESS = 'in_progress', 'In Progress'
+        COMPLETED = 'completed', 'Completed'
+
+    class PriorityChoices(models.TextChoices):
+        HIGH = 'high', 'High'
+        MEDIUM = 'medium', 'Medium'
+        LOW = 'low', 'Low'
+
+    title = models.CharField(max_length=150, null=False)
+    description = models.CharField(max_length=225, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(default=timezone.now)
+    assigned_to = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, blank=True, related_name='assigned_to')
+    status = models.CharField(max_length=20, choices=StatusChoices.choices, default=StatusChoices.OPEN)
+    priority = models.CharField(max_length=20, choices=PriorityChoices.choices, default=PriorityChoices.MEDIUM)
+
+    def __str__(self) -> str:
+        return self.title
