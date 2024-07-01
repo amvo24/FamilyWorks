b30fa6c341726851d43c77d0e8087c7a6a968f12
Andrew <andrewvo1024@gmail.com>
2024-06-30 22:48:31 -0500

    models for notification started and under way

--- FamilyWorks/notifications/models.py
+++ FamilyWorks/notifications/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Notificatiions(models.Model):
+    NOTIFICATION_TYPES = [
+        ('INVITE', 'Invitation'),
+        ('TASK', 'Task Update'),
+        ('COMMENT', 'Comment')
+    ]
+
+    recepient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent-notifications')
+    notification_type = models.CharField(max_length=20, choices=NOTIFICATION_TYPES)
+    content = models.TextField()
