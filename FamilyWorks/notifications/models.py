f03414dcdf6d8a017a63f4756cffacf4f865b080
Andrew <andrewvo1024@gmail.com>
2024-07-01 22:37:33 -0500

    model for notifications done. Utils file created but not started

--- FamilyWorks/notifications/models.py
+++ FamilyWorks/notifications/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 User = get_user_model()
 
@@ -15,3 +16,11 @@ class Notificatiions(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent-notifications')
     notification_type = models.CharField(max_length=20, choices=NOTIFICATION_TYPES)
     content = models.TextField()
+    seen = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    task = models.ForeignKey('task.Task', on_delete=models.CASCADE, null=True, blank=True)
+    family = models.ForeignKey('family.Family', on_delete=models.CASCADE, null=True, blank=True)
+
+
+    def __str__(self):
+        return f"Notification for {self.recipient} - {self.notification_type}"
