8032439333f80248483faca76fee7c75461bdd6c
Andrew <andrewvo1024@gmail.com>
2024-07-01 22:45:47 -0500

    model for comments started, must decide if this will be a generic relationship or not

--- FamilyWorks/comments/models.py
+++ FamilyWorks/comments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+
+User = get_user_model()
 
 # Create your models here.
+class Comment(models.Model):
+    comment = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    # might need to make this a generic realtionship
+    
