73d9c3c15b673ddd0501353883092e5fc1144ed0
Andrew <andrewvo1024@gmail.com>
2024-07-02 23:48:12 -0500

    model view set for comments started

--- FamilyWorks/comments/views.py
+++ FamilyWorks/comments/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Comment
+# from .serializer import [enter serializer here]
 # Create your views here.
+
+class Comment(viewsets.ModelViewSet):
+    queryset = Comment.object.all()
+    # serializer_class = serialier
+
+    
