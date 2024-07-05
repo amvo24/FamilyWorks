60c7f31db6801ff7205ee5773858c67f50b2178c
Andrew <andrewvo1024@gmail.com>
2024-07-04 22:48:50 -0500

    modelviewset for notification is done but not yet tests in postman

--- FamilyWorks/notifications/views.py
+++ FamilyWorks/notifications/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
 from .models import Notifications
+from .serializer import NotificationSerializer
 
 # Create your views here.
 class NotificationViewSet(viewsets.ModelViewSet):
     queryset = Notifications.objects.all()
-    
+    serializer_class = NotificationSerializer
