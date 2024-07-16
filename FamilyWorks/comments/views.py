04e2618b2e7c23164ad687f6def2a133cd153120
Andrew <andrewvo1024@gmail.com>
2024-07-15 21:16:47 -0500

    finished modelviewset for comments but might need to go back and add permissions

--- FamilyWorks/comments/views.py
+++ FamilyWorks/comments/views.py
@@ -1,10 +1,8 @@
 from rest_framework import viewsets
 from .models import Comment
-# from .serializer import [enter serializer here]
+from .serializer import CommentSerializer
 # Create your views here.
 
 class Comment(viewsets.ModelViewSet):
     queryset = Comment.object.all()
-    # serializer_class = serialier
-
-    
+    serializer_class = CommentSerializer
